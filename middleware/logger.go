322578dc56b90f9338b6a7b695088776882e6e4a
YixiaoGong <1213489062@qq.com>
2026-01-19 11:19:14 +0800

    v1.1

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,27 +1,24 @@
 package middleware
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Logger 是一个简单的请求日志中间件
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		path := c.Request.URL.Path
 		method := c.Request.Method
 
-		// 执行 handler
 		c.Next()
 
-		// 记录日志
 		end := time.Now()
 		latency := end.Sub(start)
 		statusCode := c.Writer.Status()
 
-		// 使用 println 输出到终端（不影响 HTTP 响应）
-		println("[GIN LOG]", method, path, "->", statusCode, "(", latency, ")")
+		fmt.Printf("[CustomGin LOG] %s %s -> %d (%v)\n", method, path, statusCode, latency)
 	}
 }
